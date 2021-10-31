63d7ca9cd81b227c87d54d2cdad2cbe71df266e4
Matias Luna <matias.luna@kavak.com>
2021-10-31 18:59:53 -0300

    Change cors properties

--- index.ts
+++ index.ts
@@ -6,10 +6,11 @@ const cors = require('cors');
 const router = express();
 
 const port = process.env.PORT || 8080;
+
+router.use(cors());
 router.listen(port, () => {
     console.log(`Server listen on port: ${port}`);
     router.use(express.json())
     router.use(morgan("dev"))
     router.use('/api', defaultRouter)
-    router.use(cors());
 })
